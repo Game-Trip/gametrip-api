3bfc7fd650aca17490d1ac9c1f75ba4d064adf70
Antoine Capitain <acapitain@elcia.com>
2023-03-30 12:00:52 +0200

    test docker tag

--- GameTrip/GameTrip.Provider/StartupProvider.cs
+++ GameTrip/GameTrip.Provider/StartupProvider.cs
@@ -4,6 +4,6 @@ namespace GameTrip.Provider
 {
     public class StartupProvider : IStartupProvider
     {
-        public string ping() => "Antoine!!!";
+        public string ping() => "UwUaaaa!!!";
     }
 }
