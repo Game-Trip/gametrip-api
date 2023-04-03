bb7c8ba7e3ed445783dd1f254ce62786de6c3a49
Dercraker <antoine.capitain@gmail.com>
2023-04-03 17:16:35 +0200

    testModel

---
+++ GameTrip/GameTrip.Domain/Models/testModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTrip.Domain.Models
+{
+    public class testModel
+    {
+        public string test { get; set; }
+
+        public testModel(string test)
+        {
+            this.test = test;
+        }
+    }
+}
