64f5fd150df35700e9095d3f8f4ea41ce962fd0a
Dercraker <antoine.capitain@gmail.com>
2023-04-13 00:38:04 +0200

    f

--- GameTrip/GameTrip.API/Program.cs
+++ GameTrip/GameTrip.API/Program.cs
@@ -9,11 +9,13 @@ public class Program
         CreateHostBuilder(args).Build().Run();
     }
 
-    private static IWebHostBuilder CreateHostBuilder(string[] args)
-    {
-        IConfigurationRoot? config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
 
-        return WebHost.CreateDefaultBuilder(args).UseConfiguration(config)
-                .UseStartup<Startup>();
-    }
+    private static IWebHostBuilder CreateHostBuilder(string[] args)
+        {
+            IConfigurationRoot? config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("GameTrip.API/appsettings.json").Build();
+
+            return WebHost.CreateDefaultBuilder(args).UseConfiguration(config)
+                    .UseStartup<Startup>();
+        }
+
 }
