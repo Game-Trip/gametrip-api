475790ac274f3679135e958a93b8905b0a298364
Dercraker <antoine.capitain@gmail.com>
2023-04-24 22:01:14 +0200

    l

--- GameTrip/GameTrip.Domain/Settings/JWTSettings.cs
+++ GameTrip/GameTrip.Domain/Settings/JWTSettings.cs
@@ -6,7 +6,7 @@ public class JWTSettings
     public string? Secret { get; set; }
     public string? ValidIssuer { get; set; }
     public string? ValidAudience { get; set; }
-    public int? DurationTime { get; set; }
+    public int DurationTime { get; set; }
 
     #endregion
 }
