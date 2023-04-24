affe5a24b3a251c6c3925fcefab2febaf13c8034
Dercraker <antoine.capitain@gmail.com>
2023-04-24 21:58:19 +0200

    k

--- GameTrip/GameTrip.Domain/Settings/JWTSettings.cs
+++ GameTrip/GameTrip.Domain/Settings/JWTSettings.cs
@@ -3,20 +3,10 @@
 public class JWTSettings
 {
     #region Properties
-    public string Secret { get; set; }
-    public string ValidIssuer { get; set; }
-    public string ValidAudience { get; set; }
-    public int DurationTime { get; set; }
+    public string? Secret { get; set; }
+    public string? ValidIssuer { get; set; }
+    public string? ValidAudience { get; set; }
+    public int? DurationTime { get; set; }
 
-    #endregion
-
-    #region Constructor
-    public JWTSettings(string secret, string validIssuer, string validAudience, int durationTime)
-    {
-        Secret = secret;
-        DurationTime = durationTime;
-        ValidIssuer = validIssuer;
-        ValidAudience = validAudience;
-    }
     #endregion
 }
