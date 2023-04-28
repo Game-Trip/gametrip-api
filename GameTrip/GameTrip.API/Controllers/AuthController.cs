7b2dd286f4770650c1f9d41f60cf462cd355ef47
Dercraker <antoine.capitain@gmail.com>
2023-04-28 22:25:12 +0200

    remove useless com

--- GameTrip/GameTrip.API/Controllers/AuthController.cs
+++ GameTrip/GameTrip.API/Controllers/AuthController.cs
@@ -14,9 +14,6 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 
 namespace GameTrip.API.Controllers;
-/// <summary>
-/// The auth controller.
-/// </summary>
 
 [Consumes("application/json")]
 [Produces("application/json")]
@@ -30,13 +27,6 @@ public class AuthController : ControllerBase
     private readonly IMailPlatform _mailPlatform;
     private readonly IEmailProvider _emailProvider;
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="AuthController"/> class.
-    /// </summary>
-    /// <param name="userManager">The user manager.</param>
-    /// <param name="authPlatform">The auth platform.</param>
-    /// <param name="mailPlatform">The mail platform.</param>
-    /// <param name="emailProvider">The email provider.</param>
     public AuthController(UserManager<GameTripUser> userManager, IAuthPlatform authPlatform, IMailPlatform mailPlatform, IEmailProvider emailProvider)
     {
         _userManager = userManager;
@@ -45,13 +35,6 @@ public class AuthController : ControllerBase
         _emailProvider = emailProvider;
     }
 
-    /// <summary>
-    /// Add first user and roles in DB if not exist
-    /// </summary>
-    /// <remarks>Need to be login with bearer token to use this endpoint</remarks>
-    /// <param name="dBInitializer">Class who provide method to init DB</param>
-    /// <returns>IActionResul</returns>
-    /// <response code="200">With message who indicate if DB Create or already exist</response>
     [HttpPost]
     [Route("Initialize")]
     public async Task<IActionResult> Initialize([FromServices] DBInitializer dBInitializer)
@@ -62,15 +45,6 @@ public class AuthController : ControllerBase
         return Ok(resultMessage);
     }
 
-    /// <summary>
-    /// EndPoint to login user with username or email and password
-    /// </summary>
-    /// <param name="dto">Class who containt Identifier and password properties</param>
-    /// <returns>Token DTO with bearer token and this expiration date</returns>
-    /// <response code="401">If identifier is not registred or password not correct</response>
-    [ProducesResponseType(typeof(TokenDTO), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(ErrorReturnDTO), StatusCodes.Status404NotFound)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
     [AllowAnonymous]
     [HttpPost]
     [Route("Login")]
@@ -92,15 +66,6 @@ public class AuthController : ControllerBase
             return Unauthorized(new ErrorReturnDTO(UserError.FailedLogin));
     }
 
-    /// <summary>
-    /// EndPoint to register user with Username, Email and Password
-    /// </summary>
-    /// <returns>GameTripUserDTO</returns>
-    /// <exception cref="FileNotFoundException">Register template mail not found</exception>
-    /// <response code="200">The user is registered and will receive an account confirmation email</response>
-    /// <response code="400">If username or email already exist</response>
-    [ProducesResponseType(typeof(GameTripUserDTO), StatusCodes.Status200OK)]
-    [ProducesResponseType(typeof(ErrorReturnDTO), StatusCodes.Status400BadRequest)]
     [AllowAnonymous]
     [HttpPost]
     [Route("Register")]
@@ -148,11 +113,6 @@ public class AuthController : ControllerBase
         return Ok(user.ToDTO());
     }
 
-    /// <summary>
-    /// Confirms the email.
-    /// </summary>
-    /// <param name="dto">The dto.</param>
-    /// <returns>A Task.</returns>
     [AllowAnonymous]
     [HttpPost]
     [Route("ConfirmEmail")]
@@ -172,11 +132,6 @@ public class AuthController : ControllerBase
         return result.Succeeded ? Ok() : Unauthorized();
     }
 
-    /// <summary>
-    /// Frogots the password.
-    /// </summary>
-    /// <param name="dto">The dto.</param>
-    /// <returns>A Task.</returns>
     [AllowAnonymous]
     [HttpPost]
     [Route("FrogotPassword")]
@@ -211,11 +166,6 @@ public class AuthController : ControllerBase
         return Ok();
     }
 
-    /// <summary>
-    /// Resets the password.
-    /// </summary>
-    /// <param name="dto">The dto.</param>
-    /// <returns>A Task.</returns>
     [AllowAnonymous]
     [HttpPost]
     [Route("ResetPassword")]
